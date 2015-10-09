65ea3755a039e2261f8d41ef3b15ecfb491bf8aa
Timothy Crosley <timothy.crosley@gmail.com>
2015-10-08 23:44:28 -0700

    First working implementation

---
+++ connectable/base.py
@@ -0,0 +1,138 @@
+'''
+    Connectable.py
+
+    Connectable enables child object to create dynamic connections
+    (via signals/slots) at run-time. Inspired by QT's signal / slot mechanism
+
+    Copyright (C) 2015  Timothy Edmund Crosley
+
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License
+    as published by the Free Software Foundation; either version 2
+    of the License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+'''
+class Connectable(object):
+    __slots__ = ("connections")
+
+    signals = []
+
+    def __init__(self):
+        self.connections = None
+
+    def emit(self, signal, value=None):
+        """
+            Calls all slot methods connected with signal,
+            optionally passing in a value
+
+            signal - the name of the signal to emit, must be defined in the classes 'signals' list.
+            value - the value to pass to all connected slot methods.
+        """
+        results = []
+        if self.connections and signal in self.connections:
+            for obj, conditions in self.connections[signal].items():
+                for condition, values in conditions.items():
+                    if condition is None or condition == value:
+                        for overrideValue, slots in values.items():
+                            if overrideValue is not None:
+                                usedValue = overrideValue
+                                if isinstance(overrideValue, str):
+                                    usedValue = usedValue.replace('${value}', str(value))
+                            else:
+                                usedValue = value
+
+                            for slot in slots:
+                                if not hasattr(obj, slot):
+                                    print(obj.__class__.__name__ +
+                                            " slot not defined: " + slot)
+                                    return False
+
+                                slotMethod = getattr(obj, slot)
+                                if usedValue is not None:
+                                    if(acceptsArguments(slotMethod, 1)):
+                                        results.append(slotMethod(usedValue))
+                                    elif(acceptsArguments(slotMethod, 0)):
+                                        results.append(slotMethod())
+                                    else:
+                                        results.append('')
+
+                                else:
+                                    results.append(slotMethod())
+
+        return results
+
+    def connect(self, signal, condition, receiver, slot, value=None):
+        """
+            Defines a connection between this objects signal
+            and another objects slot:
+
+            signal - the signal this class will emit, to cause the slot method to be called.
+            condition - only call the slot method if the value emitted matches this condition.
+            receiver - the object containing the slot method to be called.
+            slot - the name of the slot method to call.
+            value - an optional value override to pass into the slot method as the first variable.
+        """
+        if not signal in self.signals:
+            print("%(name)s is trying to connect a slot to an undefined signal: %(signal)s" %
+                      {'name':self.__class__.__name__, 'signal':str(signal)})
+            return
+
+        if self.connections is None:
+            self.connections = {}
+        connections = self.connections.setdefault(signal, {})
+        connection = connections.setdefault(receiver, {})
+        connection = connection.setdefault(condition, {})
+        connection = connection.setdefault(value, [])
+        if not slot in connection:
+            connection.append(slot)
+
+    def disconnect(self, signal=None, condition=None,
+                   obj=None, slot=None, value=None):
+        """
+            Removes connection(s) between this objects signal
+            and connected slot(s):
+
+            signal - the signal this class will emit, to cause the slot method to be called.
+            condition - only call the slot method if the value emitted matches this condition.
+            receiver - the object containing the slot method to be called.
+            slot - the name of the slot method to call.
+            value - an optional value override to pass into the slot method as the first variable.
+        """
+        if slot:
+            connection = self.connections[signal][obj][condition][value]
+            connection.remove(slot)
+        elif obj:
+            self.connections[signal].pop(obj)
+        elif signal:
+            self.connections.pop(signal, None)
+        else:
+            self.connections = None
+
+
+def acceptsArguments(method, numberOfArguments):
+    """
+        Returns True if the given method will accept the given number of arguments:
+            method - the method to perform introspection on
+            numberOfArguments - the numberOfArguments
+    """
+    if 'method' in method.__class__.__name__:
+        numberOfArguments += 1
+        func = getattr(method, 'im_func', getattr(method, '__func__'))
+        funcDefaults = getattr(func, 'func_defaults', getattr(func, '__defaults__'))
+        numberOfDefaults = funcDefaults and len(funcDefaults) or 0
+    elif method.__class__.__name__ == 'function':
+        funcDefaults = getattr(method, 'func_defaults', getattr(method, '__defaults__'))
+        numberOfDefaults = funcDefaults and len(funcDefaults) or 0
+    coArgCount = getattr(method, 'func_code', getattr(method, '__code__')).co_argcount
+    if(coArgCount >= numberOfArguments and coArgCount - numberOfDefaults <= numberOfArguments):
+        return True
+
+    return False
--- tests/test_connectable.py
+++ tests/test_connectable.py
@@ -1,29 +1,123 @@
-"""tests/test_connectable.py.
+'''
+    test_Connectable.py
 
-Tests the Python3 implementation of connectable
+    Tests the functionality of connectable
 
-Copyright (C) 2015 Timothy Edmund Crosley
+    Copyright (C) 2015  Timothy Edmund Crosley
 
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
-documentation files (the "Software"), to deal in the Software without restriction, including without limitation
-the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and
-to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License
+    as published by the Free Software Foundation; either version 2
+    of the License, or (at your option) any later version.
 
-The above copyright notice and this permission notice shall be included in all copies or
-substantial portions of the Software.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
-TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
-CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
-OTHER DEALINGS IN THE SOFTWARE.
-
-"""
-import pytest
-
-import connectable
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+'''
+from connectable import Connectable
 
 
-def test_connectable():
-    '''Test to ensure connectable works as expected'''
-    assert connectable
+class Value(Connectable):
+    signals = ['valueChanged']
+
+    def __init__(self, value):
+        super(Value, self).__init__()
+        self.value = value
+
+    def setValue(self, value):
+        self.value = value
+        self.emit('valueChanged', value)
+
+    def clearValue(self):
+        self.value = ""
+
+    def tooManyParmsToBeSlot(self, param1, param2):
+        pass
+
+
+def test_incorrectSignalSlotConnections():
+    value1, value2 = (Value(1), Value(2))
+
+    #emit fake signal
+    assert value1.emit("fake signal") == []
+
+    #connect using fake signal
+    assert value1.connect("fake signal", None, value1, 'setValue') is None
+
+    #connect to fake slot
+    value1.connect("valueChanged", None, value1, 'fake slot')
+    assert value1.emit('valueChanged') is False
+
+
+def test_connectWithoutCondition():
+    value1, value2 = (Value(1), Value(2))
+
+    #test without value overide
+    value1.connect('valueChanged', None, value2, 'setValue')
+    value1.setValue("This is a test")
+    assert value2.value == "This is a test"
+    value1.disconnect()
+
+    #test with value overide
+    value1.connect('valueChanged', None,
+                        value2, 'setValue', 'I changed the value')
+    value1.setValue("This is a test")
+    assert value2.value == "I changed the value"
+
+
+def test_connectWithCondition():
+    value1, value2 = (Value(1), Value(2))
+
+    #test without value overide
+    value1.connect('valueChanged', 'Hello', value2, 'setValue')
+    value1.setValue('Goodbye')
+    assert value2.value == 2
+    value1.setValue('Hello')
+    assert value2.value == 'Hello'
+    value1.disconnect()
+
+    #test with value overide
+    value1.connect('valueChanged', 'Hello',
+                        value2, 'setValue', 'Goodbye')
+    value1.setValue('Goodbye')
+    assert value2.value == 'Hello'
+    value1.setValue('Hello')
+    assert value2.value == 'Goodbye'
+
+    #Test on slot that takes no arguments
+    value1.connect('valueChanged', 'Die!!', value2, 'clearValue')
+    assert value1.emit('valueChanged', 'Die!!') == [None]
+    value1.connect('valueChanged', None, value2, 'clearValue')
+    assert value1.emit('valueChanged') == [None]
+    value1.disconnect()
+
+    #Test method with too many params to be a slot
+    value1.connect('valueChanged', 'False', value2, 'tooManyParmsToBeSlot')
+    assert value1.emit('valueChanged', 'False') == ['']
+
+
+def test_disconnect():
+    value1, value2 = (Value(1), Value(2))
+
+    value1.connect('valueChanged', None, value2, 'setValue')
+    value1.setValue('It changes the value')
+    assert value2.value == 'It changes the value'
+
+    value1.disconnect('valueChanged', None, value2, 'setValue')
+    value1.setValue('But not anymore')
+    assert value2.value == 'It changes the value'
+
+    value1.connect('valueChanged', None, value2, 'setValue')
+    value1.disconnect('valueChanged', None, value2)
+    value1.setValue('Still Wont')
+    assert value2.value == 'It changes the value'
+
+    value1.connect('valueChanged', None, value2, 'setValue')
+    value1.disconnect('valueChanged')
+    value1.setValue('Still Wont')
+    assert value2.value == 'It changes the value'
